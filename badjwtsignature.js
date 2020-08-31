54d0db0ca820642d5572951f310556aeae192ec2
Ismael Farfan <ifarfan@brightcove.com>
2020-08-31 10:39:15 -0500

    good and bad player

---
+++ badjwtsignature.js
@@ -0,0 +1,10 @@
+
+
+videojs('vjs_video_3').catalog.setBcovAuthToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NpZCI6IjYwNzE1NzAyMzAwMDEiLCJleHAiOjE2MDAyNzc0MTksImlhdCI6MTU5ODU0OTQxOSwicHJpZCI6Ijc4YjI3YzhhLWI4YzctNGQzYS1hNDBkLWI5NzE0ZGRlNTJkMiJ9.fbq2JFY_W7ncOiJah22fXGqgdNBg8QK6Eu4-NxLYM4__UHnwXHlsOQ4B2TLgAzyT660Wm8120xlYyHPJp-mmySrsfnWyNL5W7Hj4PDhQDjOpzIvSDxcTnRZCWO3UoF_n8EQDxlnql-RC9VsEI6A-pIeHlqQ-vmuydAHXnUdxEscs4Sgzw0Ka_UmUe0giSi1gCMgQOBGh9y5Dhlr33tU0MkZirLoPWNNOR65wxAVCxU-PfZe7uJmnmkeiyx-hHR_sKsh1gajxxyITZCmzlL-T6901VMVFCtG_JF2Si3bS3ztacSm5n5GR9G7Sur2-WRpVx0epi-57zKDPZo");
+
+
+videojs('vjs_video_3').catalog.get({id:"70700559513202", type:'video'}, (err, data) => videojs('vjs_video_3').catalog.load(data) );
+
+
+
+// https://preview-players.qa.brightcove.net/v2/accounts/6071570230001/players/TBXQUswFQb/preview/embeds/default/master/index.html?videoId=70700559513202
